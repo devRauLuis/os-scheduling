63943c06607977cfc23bb5da586f943f97f5cccf
Raul Luis <soyrauluis@gmail.com>
2023-07-02 19:03:04 -0400

    Update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,20 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: '#36413E',
-				secondary: '#5d5e60',
-				tertiary: '#8D8D92',
-				progress: '#FF2E00',
+				primary: '#503C90',
+				secondary: '#302456',
+				tertiary: '#100C1D',
+				code: '#6A52B7',
 			},
+			keyframes: {
+				'step-rotate': {
+					to: { transform: 'rotate(360deg)' },
+				},
+			},
+			animation: {
+				'step-rotate': 'step-rotate 1s steps(10) infinite',
+			},
+			fontFamily: { mono: ['"Ubuntu Mono"'] },
 		},
 	},
 	plugins: [],
